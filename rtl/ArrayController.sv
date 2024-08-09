c5348cdbb5845805c7dc602271a9cde31fd9617a
cbauzon <cjnozuab@gmail.com>
2024-08-09 13:54:13 -0700

    Instantiated buffers in controller

--- rtl/ArrayController.sv
+++ rtl/ArrayController.sv
@@ -2,14 +2,51 @@ module ArrayController(
     input i_clk,
     input i_rst_n,
 
-    input i_data_valid,
-    input i_dma_ready,
+    // Slave interface
+    input s_axis_valid,
+    output s_axis_ready,
 
-    output o_device_ready,
-    output o_data_valid
+    // Master interface
+    output m_axis_valid,
+    input m_axis_ready
 );
 
-typedef enum {IDLE, GET, PROCESS, SEND} states;
+/* BUFFERS */
+parameter DATA_WIDTH=24;
+logic buff_rst_n
+logic buff_rd, buff_wr; // both data for a and b buffers come in at same time
+
+// buffer for A matrix
+logic [DATA_WIDTH-1:0] buff_wr_data_a, o_data_a;
+logic o_is_empty_a, o_is_full_a;
+InputBuffer A_Buffer (
+    i_clk, 
+    buff_rst_n,
+    buff_rd, 
+    buff_wr, 
+    buff_wr_data_a, 
+    o_data_a, 
+    o_is_empty_a, 
+    o_is_full_a
+);
+
+// buffer for B matrix
+logic [DATA_WIDTH-1:0] buff_wr_data_b, o_data_b;
+logic o_is_empty_b, o_is_full_b;
+InputBuffer A_Buffer (
+    i_clk, 
+    buff_rst_n,
+    buff_rd, 
+    buff_wr, 
+    buff_wr_data_b, 
+    o_data_b, 
+    o_is_empty_b, 
+    o_is_full_b
+);
+
+
+/* STATE MACHINE */
+typedef enum {WAIT, FILL, PROCESS, OUT} states;
 
 logic [1:0] PS, NS;
 
